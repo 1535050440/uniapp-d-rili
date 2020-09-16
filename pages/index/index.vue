b96a7e48349d2bdfd60ffb24951795ad25028b62
1415551519 <1415551519@qq.com>
2020-09-16 16:46:25 +0800

    v1.0.0

---
+++ pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="content">
+		<view class="wrapper">
+			d-rili日历 - 可左右滑动
+		</view>
+		
+		<!--  -->
+		<d-rili></d-rili>
+		<!--  -->
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.wrapper {
+		width: 100%;
+		height: 200rpx;
+		background-color: pink;
+		color: #fff;
+		font-size: 40rpx;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	
+</style>
